0f9c8006639fc1143acf272ec4df1b7fc2212a3e
develon2015 <develon@qq.com>
2023-11-01 15:51:01 +0800

    init

---
+++ examples/balance.ts
@@ -0,0 +1,7 @@
+import web3 from "@solana/web3.js";
+
+let rpc = web3.clusterApiUrl("mainnet-beta"); // 替换为适合您的网络的 RPC 地址，如 https://api.mainnet-beta.solana.com
+let connection = new web3.Connection(rpc);
+let publicKey = new web3.PublicKey("CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S");
+let balance = await connection.getBalance(publicKey) / 1000000000; // 将 lamports 转换为 SOL
+console.log(`地址 ${publicKey} 账户余额: ${balance} SOL`);
